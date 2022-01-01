e23ef39b695b2d7913216ff4eac1893540e7e97b
Ingo Fischer <github@fischer-ka.de>
2022-01-01 23:56:03 +0100

    * (Apollon77) Adjust logging

--- main.js
+++ main.js
@@ -229,7 +229,7 @@ class HmIpCloudAccesspointAdapter extends utils.Adapter {
                             return;
                     }
                     const pin = await this.getStateAsync('devices.' + o.native.id + '.channels.' + o.native.channel + '.pin');
-                    this.log.info(`Call setLockState for ${state.val} with PIN ${pin}`);
+                    this.log.info(`Call setLockState for ${state.val} with PIN ${pin ? pin.val : ''}`);
                     await this._api.deviceControlSetLockState(o.native.id, state.val, pin ? pin.val : '', o.native.channel);
                     break;
                 case 'resetEnergyCounter':
