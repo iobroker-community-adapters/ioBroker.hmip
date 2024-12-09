9035959a6cf993e52251430cab441d04c53ec576
Martin M. <mcm57@gmx.at>
2024-12-09 14:01:20 +0100

    Go for standard eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,27 @@
+// ioBroker eslint template configuration file for js and ts files
+// Please note that esm or react based modules need additional modules loaded.
 import config from '@iobroker/eslint-config';
 
 export default [
     ...config,
+
     {
-        languageOptions: {
-            parserOptions: {
-                projectService: {
-                    allowDefaultProject: ['*.mjs', 'api/*.js', '*.js'],
-                },
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
+        // specify files to exclude from linting here
+        ignores: [
+            '*.test.js',
+            '*.config.mjs',
+            'node_modules',
+            'test/',
+            'admin/',
+            'src-admin',
+        ],
     },
+
     {
+        // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
+        // as this improves maintainability. jsdoc warnings will not block buiuld process.
         rules: {
-            '@typescript-eslint/no-require-imports': 'off',
             'jsdoc/require-jsdoc': 'off',
-            'jsdoc/require-param': 'off',
         },
-    },
-    {
-        ignores: ['node_modules/', 'admin/', 'src-admin/', 'test/'],
     },
 ];
